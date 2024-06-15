84bfd2c4b2700aaa00e127e63a85e45701c713be
Tosi-eu <galdiniguilherme@usp.br>
2024-06-15 16:12:51 -0300

    Optimization: fix tests again

--- tests/unitary_test.py
+++ tests/unitary_test.py
@@ -2,7 +2,9 @@ import pytest
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from app import app
+from app import app, handle_candidatura_insertion, delete_from_db
+import unittest
+from unittest.mock import patch, Mock
 
 @pytest.fixture
 def client():
@@ -13,37 +15,44 @@ def test_default_page(client):
     response = client.get('/teste')
     assert response.status_code == 404
 
-def test_index():
-    with app.test_client() as client:
-        response = client.get('/')
-        assert response.status_code == 200
+def test_handle_candidatura_insertion():
+    with patch('app.get_db_connection') as mock_get_db_connection:
+        mock_conn = Mock()
+        mock_cursor = Mock()
+        mock_get_db_connection.return_value = mock_conn
+        mock_conn.cursor.return_value = mock_cursor
 
-def test_get_eleitos():
-    with app.test_client() as client:
-        response = client.get('/candidaturas/eleitos')
-        assert response.status_code == 200
+        form_data = {
+            'cod_candidatura': '1',
+            'cod_individuo': '12345678901',
+            'cod_cargo': '2',
+            'cod_Partido': '3',
+            'ano': '2024',
+            'pleito': '4',
+            'cod_candidatura_vice': '5',
+            'eleito': 'SIM',
+            'total_doacoes': '1000'
+        }
 
-def test_list_candidaturas():
-    with app.test_client() as client:
-        response = client.get('/candidaturas')
-        assert response.status_code == 200
+        handle_candidatura_insertion(mock_cursor, form_data)
 
-def test_get_ficha_limpa():
-    with app.test_client() as client:
-        response = client.get('/candidatos/ficha-limpa')
-        assert response.status_code == 200
+        mock_cursor.execute.assert_called_once()
+        mock_conn.commit.assert_called_once()
 
-def test_delete_entity():
-    with app.test_client() as client:
-        response = client.get('/delete')
-        assert response.status_code == 200
+def test_delete_from_db():
+    with patch('app.get_db_connection') as mock_get_db_connection:
+        mock_conn = Mock()
+        mock_cursor = Mock()
+        mock_get_db_connection.return_value = mock_conn
+        mock_conn.cursor.return_value = mock_cursor
 
-def test_inserir():
-    with app.test_client() as client:
-        response = client.get('/inserir')
-        assert response.status_code == 200
+        table = 'Candidatura'
+        id_column = 'Cod_Candidatura'
+        entity_id = '1'
+        entity = 'candidatura'
 
-def test_doacoes():
-    with app.test_client() as client:
-        response = client.get('/doacoes')
-        assert response.status_code == 200
+        message = delete_from_db(table, id_column, entity_id, entity)
+
+        mock_cursor.execute.assert_called_once()
+        mock_conn.commit.assert_called_once()
+        assert message == 'Candidatura com ID 1 removido com sucesso.'

95d2caad6415e893b2fb813993c0d4722371396d
Tosi-eu <galdiniguilherme@usp.br>
2024-06-15 16:30:15 -0300

    Optimization: fix tests again

--- tests/unitary_test.py
+++ tests/unitary_test.py
@@ -2,10 +2,63 @@ from unittest.mock import MagicMock
 import pytest
 import os
 import sys
-
-from static.misc import is_valid_entity, is_valid_id, get_table_and_column, handle_pleito_insertion
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app
+
+def is_valid_entity(entity):
+    valid_entities = [
+        'pleito', 'candidatura', 'cargo', 'individuo',
+        'equipeapoio', 'doadoresf', 'doadoresj',
+        'processojudicial', 'empresa'
+    ]
+    return entity in valid_entities
+
+def is_valid_id(entity, id):
+    if entity == 'individuo' and len(id) != 14:
+        return False
+    if entity == 'empresa' and len(id) != 18:
+        return False
+    return True
+
+def get_invalid_message(entity):
+    if entity == 'individuo':
+        return "CPF inválido ou não encontrado!"
+    if entity == 'empresa':
+        return "CNPJ inválido ou não encontrado!"
+    return "Entidade ou coluna de ID inválida."
+
+def get_table_and_column(entity):
+    table_mapping = {
+        'pleito': 'Pleito',
+        'candidatura': 'Candidatura',
+        'cargo': 'Cargo',
+        'individuo': 'Individuo',
+        'equipeapoio': 'EquipeApoio',
+        'doadoresf': 'DoacaoPF',
+        'doadoresj': 'DoadorPJ',
+        'processojudicial': 'ProcessoJudicial',
+        'empresa': 'empresa'
+    }
+    id_column_mapping = {
+        'pleito': 'Cod_Pleito',
+        'candidatura': 'Cod_Candidatura',
+        'individuo': 'CPF',
+        'cargo': 'Cod_Cargo',
+        'equipeapoio': 'Cod_Equipe',
+        'doadoresf': 'Cod_Nota',
+        'doadoresj': 'Cod_Candidatura',
+        'processojudicial': 'Cod_Processo',
+        'empresa': 'cnpj'
+    }
+    return table_mapping.get(entity), id_column_mapping.get(entity)
+
+## INSERÇÕES
+
+def handle_pleito_insertion(cursor, form):
+    cod_pleito = form['Cod_Pleito']
+    qtd_votos = form['qtdVotos']
+    query = "INSERT INTO Pleito (Cod_Pleito, Qtd_Votos) VALUES (%s, %s)"
+    cursor.execute(query, (cod_pleito, qtd_votos))
 
 @pytest.fixture
 def client():
